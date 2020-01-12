f1dcde5449af05ccbecae3e5ff8c75a074eaab67
Emanuel Posescu <emanuel.posescu@dacodasoft.ro>
2020-01-12 12:02:45 +0200

    Store IP settings in RTC after first boot

--- esp32-analog-intercom.ino
+++ esp32-analog-intercom.ino
@@ -1,22 +1,55 @@
 #include "config.h"
-#include <U8g2lib.h>
 
+#include <U8g2lib.h>
 U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE, DISPLAY_CLOCK, DISPLAY_DATA);
 U8G2LOG display;
 uint8_t u8log_buffer[U8LOG_WIDTH*U8LOG_HEIGHT];
 
-
 #include <WiFi.h>
-IPAddress staticIP(192,168,1,36);
-IPAddress gateway(192,168,1,1);
-IPAddress subnet(255,255,255,0);
-
 
 #define Threshold 40 /* Greater the value, more the sensitivity */
 
-
 RTC_DATA_ATTR int bootCount = 0;
+RTC_DATA_ATTR uint8_t ip[4];
+RTC_DATA_ATTR uint8_t gateway[4];
+RTC_DATA_ATTR uint8_t subnet[4];
 
+/**
+ * At first boot, we use DHCP and store the ip/gw/subnet values in RTC
+ * to use them during the next wakeups
+ */
+void startWifi() {
+  display.printf("Connecting to %s", ssid);
+  display.setRedrawMode(1);
+  WiFi.begin(ssid, password);
+  if(bootCount != 0) { // restore settings from RTC
+    WiFi.config(IPAddress(ip), IPAddress(gateway), IPAddress(subnet));
+  }
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(50);
+    display.print(".");
+  }
+  display.setRedrawMode(0);
+  display.println();
+  if(bootCount == 0) { // store settings in RTC
+    display.print("IP: "); display.println(WiFi.localIP());
+    display.print("GW: "); display.println(WiFi.gatewayIP());
+    display.print("NET:"); display.println(WiFi.subnetMask());
+    ip[0] = WiFi.localIP()[0];
+    ip[1] = WiFi.localIP()[1];
+    ip[2] = WiFi.localIP()[2];
+    ip[3] = WiFi.localIP()[3];
+    gateway[0] = WiFi.gatewayIP()[0];
+    gateway[1] = WiFi.gatewayIP()[1];
+    gateway[2] = WiFi.gatewayIP()[2];
+    gateway[3] = WiFi.gatewayIP()[3];
+    subnet[0] = WiFi.subnetMask()[0];
+    subnet[1] = WiFi.subnetMask()[1];
+    subnet[2] = WiFi.subnetMask()[2];
+    subnet[3] = WiFi.subnetMask()[3];
+  }
+}
 
 void print_wakeup_reason(){
   esp_sleep_wakeup_cause_t wakeup_reason;
@@ -40,8 +73,6 @@ void callback(){
 
 
 void setup() {
-  Serial.begin(115200);
-
   u8g2.begin();
   u8g2.setFont(u8g2_font_tom_thumb_4x6_mf);
   display.begin(u8g2, U8LOG_WIDTH, U8LOG_HEIGHT, u8log_buffer);
@@ -51,23 +82,13 @@ void setup() {
   if(bootCount == 0) {
   }
 
-  ++bootCount;
   display.println("Boot number: " + String(bootCount));
 
   print_wakeup_reason();
 
-  display.printf("Connecting to %s", ssid);
-  display.setRedrawMode(1);
-  WiFi.begin(ssid, password);
-  WiFi.config(staticIP, gateway, subnet);
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(50);
-    display.print(".");
-  }
-  display.setRedrawMode(0);
-  display.println();
-  display.println(WiFi.localIP());
+  startWifi();
+
+  ++bootCount;
 
   //Setup interrupt on Touch Pad 3 (GPIO15)
   touchAttachInterrupt(T3, callback, Threshold);
